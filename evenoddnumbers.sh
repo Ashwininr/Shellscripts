9caeb0b4668953a81471842d006ab2f425d46922
Ashwininr <ashwininru@gmail.com>
2024-09-21 18:13:55 +0530

    updated script

--- evenoddnumbers.sh
+++ evenoddnumbers.sh
@@ -2,7 +2,7 @@
 read -p "Enter a number and I will check if its odd or even " mynumber
 if [ $((mynumber%2)) -eq 0 ]
 then
-echo "Your number is even"
+echo "Your number is even number"
 else
-echo "Your number is odd."
+echo "Your number is odd number."
 fi
